95e73dcd32a9704f17af9f26fcfc83a8bbb7f3bc
NeoJhonn <jhonny.azevedo@gmail.com>
2024-06-21 01:36:18 -0300

    Adicionado base do projeto

---
+++ src/main/java/br/com/jhonny_azevedo/bootcamp_java_dio/SantanderBootcampJavaDioApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhonny_azevedo.bootcamp_java_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampJavaDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampJavaDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jhonny_azevedo/bootcamp_java_dio/SantanderBootcampJavaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhonny_azevedo.bootcamp_java_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampJavaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
